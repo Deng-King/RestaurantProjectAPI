b5f16f7e6982b96ab933c6a5413c97dc7175b020
bob <1313@163.com>
2021-07-09 16:37:47 +0800

    16.37

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import uvicorn
 from fastapi import FastAPI
 from starlette.middleware.cors import CORSMiddleware
+from api import login
 
 app = FastAPI()
 app.add_middleware(
@@ -10,9 +11,13 @@ app.add_middleware(
     allow_methods=["*"],
     allow_headers=["*"])
 
+app.include_router(login.router, prefix="/api")
+
+
 @app.get("/")
 async def home_info():
-    return {"home":"welcome"}
+    return {"home": "welcome"}
+
 
 if __name__ == '__main__':
     uvicorn.run(app='main:app', host="127.0.0.1", port=8000, reload=True, debug=True)
