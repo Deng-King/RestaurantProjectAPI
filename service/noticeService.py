2e0de8a9e5e90130119a77693765679310109f0b
bob <1313@163.com>
2021-07-10 14:29:31 +0800

    notice_complete

--- service/noticeService.py
+++ service/noticeService.py
@@ -7,4 +7,14 @@ def get_notice():
     notice_list = notice_showall.show(settings.ip)
     if notice_list == None:
         return responseCode.resp_4xx(code=404, message='没有新的公告可以显示')
-    return responseCode.resp_200(data=notice_list)
+    notice_dic_list = []
+    for i in notice_list:
+        notice_dic_list.append({
+            "notice_id": i[0],
+            "user_id": i[1],
+            "content": i[2],
+            "title": i[3],
+            "notice_level": i[4],
+            "notice_create_time": i[5]
+        })
+    return responseCode.resp_200(data=notice_dic_list)
