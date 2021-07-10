3fc1336238377e526af3158a02c89637712b0ff9
bob <1313@163.com>
2021-07-10 16:02:45 +0800

    格式化

--- schemas.py
+++ schemas.py
@@ -9,9 +9,11 @@ class LoginInfo(BaseModel):
 class ProfilesExit(BaseModel):
     user_id: int
 
+
 class Profiles(BaseModel):
     # 用户编号
     user_id: int
+
 
 class ProfilesEdit(BaseModel):
     # user_id_a, user_id_b, tag, content
@@ -19,6 +21,8 @@ class ProfilesEdit(BaseModel):
     user_id_b: int
     tag: int
     content: str
+
+
 class PostNoticeInfo(BaseModel):
     user_id: int
     content: str
