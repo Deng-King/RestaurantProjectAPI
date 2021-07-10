dddc43d8f4407136696b7ce7f64eb79cefded77f
bob <1313@163.com>
2021-07-10 18:00:38 +0800

    admin修改

--- api/adminApi.py
+++ api/adminApi.py
@@ -6,24 +6,24 @@ adminRouter = APIRouter()
 
 
 # 4.1 发布公告消息
-@adminRouter.post("api/announcement/post", tags=["admin"])
+@adminRouter.post("/announcement/post", tags=["admin"])
 async def post_notice(notice_info: schemas.PostNoticeInfo):
     return adminService.post_notice(notice_info)
 
 
 # 4.8 管理员添加新品
-@adminRouter.post("api/admin/meals/add", tags=["admin"])
+@adminRouter.post("/admin/meals/add", tags=["admin"])
 async def add_meal(meal: schemas.FoodInfo):
     return adminService.add_meal(meal)
 
 
 # 4.9 管理员删除菜品信息
-@adminRouter.post("api/admin/meals/remove", tags=["admin"])
+@adminRouter.post("/admin/meals/remove", tags=["admin"])
 async def remove_meal(meal_id:int):
     return adminService.remove_meal(meal_id)
 
 
 # 4.10 管理员修改菜品
-@adminRouter.post("api/admin/meals/modify",tags=["admin"])
+@adminRouter.post("/admin/meals/modify",tags=["admin"])
 async def modify_meal(mod:schemas.ModifyMeal):
     return adminService.modify_meal(mod)
