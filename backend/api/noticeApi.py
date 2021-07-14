0c0856dfde36f3df5fbb3d315e63bfdd3b59a13b
DengKing <dengki@vip.qq.com>
2021-07-14 14:38:48 +0800

    修bug

--- backend/api/noticeApi.py
+++ backend/api/noticeApi.py
@@ -5,6 +5,6 @@ noticeRouter = APIRouter()
 
 
 # 1.2获取公告消息功能
-@noticeRouter.get("/announcement/fetch", tags=["notice"])
+@noticeRouter.get("api/announcement/fetch", tags=["notice"])
 async def get_notice():
     return noticeService.get_notice()
